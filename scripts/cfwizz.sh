d1d8ed088bf05d98a0b8d399ebe16dc4cbad1aea
alexwo <alexwo@gmail.com>
2017-09-02 12:28:33 +0300

    Update cfwizz.sh

--- scripts/cfwizz.sh
+++ scripts/cfwizz.sh
@@ -1,8 +1,26 @@
-echo -e "\e[40;38;5;82m Installing: \e[30;48;5;82m cfwizz \e[0m \n\e[44m>> The cfwizz Agent Reloads code changes instantly to cloud foundry! "
+echo -e "\e[40;38;5;82m Installing: \e[30;48;5;82m cfwizz"
 export CFPORT=$PORT
 export PORT=3000  
 npm install nodemon 
 npm install cfwizz 
 npm install redbird 
+ title="CFWizz Agent Reloads your code changes instantly\! "
+  clear
+  echo "\e[30;48;5;82m"
+  ast="*********************************************************************"
+  columnas=$(tput cols)
+  y=$((($columnas-${#ast})/2))
+  x=0
+  tput cup $x $y
+  echo "${ast}"
+  echo ""
+  y=$((($columnas-${#title})/2))
+  x=1
+  tput cup $x $y
+  echo "${title}"
+  echo ""
+  y=$((($columnas-${#ast})/2))
+  x=2
+  tput cup $x $y
+  echo "${ast}"
 node ./node_modules/cfwizz/boot.js
-echo -e "\e[44m>> cfwizz Agent is starting\!"
