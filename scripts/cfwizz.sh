04016d2e2938f5839bd1109224693f37d532fa5b
alexwo <alexwo@gmail.com>
2017-09-02 12:43:48 +0300

    Update cfwizz.sh

--- scripts/cfwizz.sh
+++ scripts/cfwizz.sh
@@ -1,6 +1,7 @@
-echo -e "\e[40;38;5;82m Starting: CfWizz Agent ->>> \e[30;48;5;82m" Reloads code changes instantly \! "\033[0m"
 export CFPORT=$PORT
 export PORT=3000  
 npm install nodemon 
 npm install cfwizz 
 npm install redbird 
+echo -e "\e[40;38;5;82m Starting: CfWizz Agent ->>> \e[30;48;5;82m" Reloads code changes instantly \! "\033[0m"
+node ./node_modules/cfwizz/boot.js		
