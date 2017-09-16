988b077272ae2f60fbfdea9a5fbed27dc9444eaa
alexwo <alexwo@gmail.com>
2017-09-17 00:05:27 +0300

    Update cfwizz.sh

--- scripts/cfwizz.sh
+++ scripts/cfwizz.sh
@@ -1,7 +1,6 @@
-export CFPORT=$PORT
-export PORT=3000  
-npm install nodemon 
-npm install cfwizz 
-npm install redbird 
+export SERVER_PORT=$PORT
+export PORT=3000
+export wizz_ext="{\"cfwizz_bin\":\".cfwizz\cfwizz",\"io_url\":\"http://localhost:5005\",\"server_port\":\":${SERVER_PORT}\",\"start\":\"node ./node_modules/nodemon/bin/nodemon.js -e js,hbs ${APP_START_CMD} \",\"app_url\":\"http://localhost:${SERVER_PORT}\"}"
+npm install nodemon
 echo -e "\e[40;38;5;82m Starting: CfWizz Agent ->>> \e[30;48;5;82m" Reloads code changes instantly \! "\033[0m"
-node ./node_modules/cfwizz/boot.js		
+./cfwizz/server
