babe9ab0ba9eebbfbe4bde664be82cf6858536a0
alexwo <alexwo@gmail.com>
2017-09-17 00:42:17 +0300

    Update cfwizz.sh

--- scripts/cfwizz.sh
+++ scripts/cfwizz.sh
@@ -11,5 +11,5 @@ chmod 777 cfwizz server
 cd ..
 
 echo -e "\e[40;38;5;82m Starting: CfWizz Agent ->>> \e[30;48;5;82m" Reloads code changes instantly \! "\033[0m"
-./.cfwizz/server
+.cfwizz/server
 
