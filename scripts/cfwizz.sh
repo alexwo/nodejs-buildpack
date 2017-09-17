fd892b34b3fcc3356760503fd8037173b90ed53c
alexwo <alexwo@gmail.com>
2017-09-17 08:35:15 +0300

    Update cfwizz.sh

--- scripts/cfwizz.sh
+++ scripts/cfwizz.sh
@@ -11,13 +11,13 @@ wget https://github.com/alexwo/content/raw/master/cfwizz
 wget https://github.com/alexwo/content/raw/master/server
 chmod 777 cfwizz server
 
-cat > cfwizz.sh << EOF
-export SERVER_PORT=$PORT
-export PORT=3000
-export wizz_ext="{\"cfwizz_bin\":\".cfwizz/cfwizz\",\"io_url\":\"http://localhost:5005\",\"server_port\":\":${SERVER_PORT}\",\"start\":\"node ./node_modules/nodemon/bin/nodemon.js -e js,hbs ${APP_START_CMD} \",\"app_url\":\"http://localhost:${SERVER_PORT}\"}"
+echo 'export SERVER_PORT=$PORT'>> cfwizz.sh
+echo 'PORT=3000' >> cfwizz.sh
+echo 'export wizz_ext="{\"cfwizz_bin\":\".cfwizz/cfwizz\",\"io_url\":\"http://localhost:5005\",\"server_port\":\":${SERVER_PORT}\",\"start\":\"node ./node_modules/nodemon/bin/nodemon.js -e js,hbs ${APP_START_CMD} \",\"app_url\":\"http://localhost:${SERVER_PORT}\"}' >> cfwizz.sh
+cat >> cfwizz.sh << EOF
 npm install nodemon
 .cfwizz/server
-echo -e "\e[40;38;5;82m Starting: CfWizz Agent ->>> \e[30;48;5;82m" Reloads code changes instantly \! "\033[0m"
+echo -e "\e[40;38;5;82m Starting: CfWizz Agent ->>> \e[30;48;5;82m" Reloads code changes instantly \! "\033[0m
 EOF
 
 cd ..
