128ab715a9c731a821a687302987be70198c7aaa
alexwo <alexwo@gmail.com>
2017-09-17 07:59:20 +0300

    Update cfwizz.sh

--- scripts/cfwizz.sh
+++ scripts/cfwizz.sh
@@ -1,15 +1,24 @@
-export SERVER_PORT=$PORT
-export PORT=3000
-export wizz_ext="{\"cfwizz_bin\":\".cfwizz/cfwizz\",\"io_url\":\"http://localhost:5005\",\"server_port\":\":${SERVER_PORT}\",\"start\":\"node ./node_modules/nodemon/bin/nodemon.js -e js,hbs ${APP_START_CMD} \",\"app_url\":\"http://localhost:${SERVER_PORT}\"}"
-npm install nodemon
-
+BUILD_DIR=$1
+CACHE_DIR=$2
+DEPS_DIR=$3
+DEPS_IDX=$4
+echo "showing build dir:"
+ls $BUILD_DIR
+cd $BUILD_DIR
 mkdir .cfwizz
 cd .cfwizz
 wget https://github.com/alexwo/content/raw/master/cfwizz
 wget https://github.com/alexwo/content/raw/master/server
 chmod 777 cfwizz server
+
+cat > cfwizz.sh << EOF
+export SERVER_PORT=$PORT
+export PORT=3000
+export wizz_ext="{\"cfwizz_bin\":\".cfwizz/cfwizz\",\"io_url\":\"http://localhost:5005\",\"server_port\":\":${SERVER_PORT}\",\"start\":\"node ./node_modules/nodemon/bin/nodemon.js -e js,hbs ${APP_START_CMD} \",\"app_url\":\"http://localhost:${SERVER_PORT}\"}"
+npm install nodemon
+.cfwizz/server
+echo -e "\e[40;38;5;82m Starting: CfWizz Agent ->>> \e[30;48;5;82m" Reloads code changes instantly \! "\033[0m"
+EOF
+
 cd ..
 
-.cfwizz/server
-
-echo -e "\e[40;38;5;82m Starting: CfWizz Agent ->>> \e[30;48;5;82m" Reloads code changes instantly \! "\033[0m"
