3b61c48c05d3bed2e065b8a0e2dff05cffb0ffb8
alexwo <alexwo@gmail.com>
2017-09-17 09:03:00 +0300

    Update cfwizz.sh

--- scripts/cfwizz.sh
+++ scripts/cfwizz.sh
@@ -13,7 +13,7 @@ chmod 777 cfwizz server
 
 echo 'export SERVER_PORT=$PORT'>> cfwizz.sh
 echo 'PORT=3000' >> cfwizz.sh
-echo 'export wizz_ext="{\"cfwizz_bin\":\".cfwizz/cfwizz\",\"io_url\":\"http://localhost:5005\",\"server_port\":\":${SERVER_PORT}\",\"start\":\"node ./node_modules/nodemon/bin/nodemon.js -e js,hbs ${APP_START_CMD} \",\"app_url\":\"http://localhost:${SERVER_PORT}\"}"' >> cfwizz.sh
+echo 'export wizz_ext="{\"cfwizz_bin\":\".cfwizz/cfwizz\",\"io_url\":\"http://localhost:5005\",\"server_port\":\":${SERVER_PORT}\",\"start\":\"node ./node_modules/nodemon/bin/nodemon.js -e js,hbs ${APP_START_CMD} \",\"app_url\":\"http://localhost:${PORT}\"}"' >> cfwizz.sh
 cat >> cfwizz.sh << EOF
 npm install nodemon
 .cfwizz/server
