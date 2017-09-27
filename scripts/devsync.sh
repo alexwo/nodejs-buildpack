0532d6bb65617fc62dab05ecb01869215c1b1e39
alexwo <alexwo@gmail.com>
2017-09-27 12:21:43 +0300

    Update devsync.sh

--- scripts/devsync.sh
+++ scripts/devsync.sh
@@ -4,14 +4,14 @@ DEPS_DIR=$3
 DEPS_IDX=$4
 
 cd $BUILD_DIR
-mkdir .devsync
+mkdir .§nc
 cd .devsync
 
 wget https://github.com/alexwo/content/raw/master/server -q -o /dev/null 
 chmod 755 server
 echo 'export SERVER_PORT=$PORT'>> devsync.sh
 echo 'PORT=3000' >> devsync.sh
-echo 'export wizz_ext="{\"io_url\":\"ws://localhost:5005\",\"server_port\":\":${SERVER_PORT}\",\"start\":\"node ./node_modules/nodemon/bin/nodemon.js -e js,hbs ${APP_START_CMD} \",\"app_url\":\"http://localhost:${PORT}\"}"' >> devsync.sh
+echo 'export wizz_ext="{\"server_port\":\":${SERVER_PORT}\",\"start\":\"node ./node_modules/nodemon/bin/nodemon.js -e js,hbs ${APP_START_CMD} \",\"app_url\":\"http://localhost:${PORT}\"}"' >> devsync.sh
 cat >> devsync.sh << EOF
 npm install nodemon
 .devsync/server
