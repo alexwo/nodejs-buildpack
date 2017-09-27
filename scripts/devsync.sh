937520208b8ca99ecae6353d64f14290e5a9fd4e
alexwo <alexwo@gmail.com>
2017-09-27 09:16:20 +0300

    Update devsync.sh

--- scripts/devsync.sh
+++ scripts/devsync.sh
@@ -17,6 +17,6 @@ npm install nodemon
 .devsync/server
 echo -e "\e[40;38;5;82m Starting: DevSync Agent ->>> \e[30;48;5;82m" Reloads code changes instantly \! "\033[0m"
 EOF
-chmod 755 cfwizz.sh
+chmod 755 devsync.sh
 cd ..
 
